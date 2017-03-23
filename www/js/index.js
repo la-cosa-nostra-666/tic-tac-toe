7771f15a165604481abadd534f5f5c7d614978d0
Pawel Laski <zniwus@gmail.com>
2017-03-23 23:26:17 +0100

    remove www

--- www/js/index.js
+++
@@ -1,8 +0,0 @@
-var app = {
-    // Application Constructor
-    initialize: function() {
-      document.addEventListener('deviceready', function() {
-        console.log('ready');
-      }, false);
-    }
-};
