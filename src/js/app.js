a3f64a6c5e81ded5f523bdf8d3125485bd5ea047
Pawel Laski <zniwus@gmail.com>
2017-03-25 16:07:36 +0100

    init tile

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,16 @@
 import '../css/index.scss';
 import Header from './header';
+import Tile from './tile';
 
 export default {
   ready: () => {
     const header = new Header();
     document.body.appendChild(header.container);
+    const tile = new Tile(['without-bottom']);
+    document.body.appendChild(tile.container);
+    tile.on('click', () => {
+      tile.setPlayer('x');
+    })
     header.setCurrentPlayer('ali')
   }
 }
---
+++ src/js/tile.js
@@ -0,0 +1,26 @@
+import '../css/tile.scss'
+import EventEmitter from 'events';
+
+export default class Tile extends EventEmitter {
+  container = document.createElement('div');
+  playerContainer = document.createElement('div');
+  constructor(withoutBorders) {
+    super()
+    this.container.classList.add('tile');
+    this.container.classList.add.apply(this.container.classList, withoutBorders);
+    this.container.appendChild(this.playerContainer)
+    this.playerContainer.classList.add('player')
+    this.container.addEventListener('click', () => this.emit('click'));
+  }
+  setPlayer(player) {
+    if (!(player === 'o' || player === 'x')) {
+      return console.error(`wrong player ${player}`);
+    }
+    let classMap = {
+      'o': 'fa-circle-o',
+      'x': 'fa-times'
+    }
+    this.playerContainer.classList.remove('fa-circle-o', 'fa-times');
+    this.playerContainer.classList.add('fa', classMap[player]);
+  }
+}
