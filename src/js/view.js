50f8fb851968b5707e758673d07833d21b34cc1e
Pawel Laski <zniwus@gmail.com>
2017-03-28 15:34:14 +0200

    add restyle call after show

--- src/js/view.js
+++ src/js/view.js
@@ -3,11 +3,17 @@ import EventEmitter from 'events';
 export default class View extends EventEmitter {
   toggle(force) {
     this.container.classList.toggle('hidden', force);
+    if (force == false) {
+      this.restyle();
+    }
   }
   hide() {
     this.toggle(true);
   }
   show() {
     this.toggle(false);
+  }
+  restyle() {
+
   }
 }
