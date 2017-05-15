121bd05addf0e53d067b2f6d428a22521df3f79a
Pawel Laski <zniwus@gmail.com>
2017-05-15 15:39:57 +0200

    update cordova and fix build problems

--- scripts/android/prepare.js
+++ scripts/android/prepare.js
@@ -1,14 +1,12 @@
 const path = require('path');
 
 const exec = require('../core/exec');
-const copyRes = require('./copy-res');
 
 const prepare = () => {
   return exec('rm', ['-rf', path.resolve('./www')])
   .then(() => exec('rm', ['-rf', path.resolve('./res')]))
   .then(() => exec('webpack', ['-p']))
   .then(() => exec('cordova', ['prepare']))
-  .then(() => copyRes())
 }
 
 if (require.main === module) {
